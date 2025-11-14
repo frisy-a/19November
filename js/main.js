c6178e3631a6b3bfa0196daf180d2b8709df867f
frisy-a <anhugraha@gmail.com>
2025-11-14 15:04:25 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Selamat').split('');
+    const titles = ('Selamat').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
