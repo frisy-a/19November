4a114f32b9e646cc1fccecb0fe328e52279a0f85
frisy-a <anhugraha@gmail.com>
2025-11-15 10:05:06 +0700

    script.js

--- script.js
+++ script.js
@@ -120,15 +120,15 @@ function extinguishFlame() {
 
     // Tampilkan pesan
     message.classList.remove('hidden');
-    message.textContent = "SELAMAT";
-    //{
-    //var a = document.createElement('a');
-    //var linkText = document.createTextNode("Selamat lilinnya sudah padam! Buka Hadiahnya Di Sini");
-     // a.appendChild(linkText);
-      //a.title = " Selamat lilinnya sudah padam! Buka Hadiahnya Di Sini";
-      //a.href = "https://frisy-a.github.io/19November/flower.html";
-      //document.body.appendChild(a);
-    //}
+   // message.textContent = "SELAMAT";
+    {
+    var a = document.createElement('a');
+    var linkText = document.createTextNode("Selamat lilinnya sudah padam! Buka Hadiahnya Di Sini");
+      a.appendChild(linkText);
+      a.title = " Selamat lilinnya sudah padam! Buka Hadiahnya Di Sini";
+      a.href = "https://frisy-a.github.io/19November/flower.html";
+      document.body.appendChild(a);
+    }
       // Putar lagu ulang tahun
     const birthdaySong = document.getElementById('birthdaySong');
     birthdaySong.play().catch((error) => {
@@ -148,6 +148,7 @@ function extinguishFlame() {
 
 
 window.onload = initMic;
+
 
 
 
